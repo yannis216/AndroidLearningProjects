e0cd1f9e9989e0e77b7862fb823379eabdddfccb
yannis216 <yannisbeuke@googlemail.com>
2017-10-02 08:59:41 +0200

    new java methods

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -25,29 +25,27 @@ public class MainActivity extends AppCompatActivity {
     /** This method is called when the increment Button is clicked */
     public void increment(View view) {
         numberOfCoffees = numberOfCoffees + 1;
-        display(numberOfCoffees);
+        displayQuantity(numberOfCoffees);
     }
 
     /** This method is called when the decrement Button is clicked */
     public void decrement(View view) {
         numberOfCoffees = numberOfCoffees - 1;
-        display(numberOfCoffees);
+        displayQuantity(numberOfCoffees);
     }
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = numberOfCoffees * priceOfCoffee;
-        String priceMessage = "Total: " + price + "€ \nThis is the price for " + numberOfCoffees + " Coffees";
-        displayMessage(priceMessage);
+        int finalPrice = calculatePrice(numberOfCoffees, priceOfCoffee);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number2) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        quantityTextView.setText("" + number2);
     }
 
     /**
@@ -64,5 +62,14 @@ public class MainActivity extends AppCompatActivity {
     private void displayMessage(String message) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(message);
+    }
+    /**
+     * This method calculates the price for the Coffes
+     * @param price is the price of the coffees
+     * @param quantity is the number of coffees ordered
+     */
+    private int calculatePrice(int quantity, int price){
+        int finalPrice= price*quantity;
+        return finalPrice;
     }
 }
